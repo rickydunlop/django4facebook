a74fbbd6c822b1384d9cd5f1501c8a01fb2ed9fe
Venelin Stoykov <vkstoykov@gmail.com>
2012-10-25 15:14:54 +0200

    Update deauthorization callback
    Deauthorization callback must not relay on FacebookMiddleware

--- django4facebook/views.py
+++ django4facebook/views.py
@@ -1,13 +1,23 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+
+import facebook
+from .conf import settings
 
 
+@csrf_exempt
 def deauthorize_callback(request):
     """
     When user deauthorize this application from facebook then
     we deactivate the user from our system
     """
-    if not request.facebook:
+    signed_request = request.REQUEST.get('signed_request')
+    if not signed_request:
         return HttpResponseBadRequest()
-    User.objects.filter(pk=request.facebook.uid).update(is_active=False)
+    data = facebook.parse_signed_request(signed_request,
+                                             settings.SECRET_KEY)
+    if not data.get('user_id'):
+        return HttpResponseBadRequest()
+    User.objects.filter(pk=data['user_id']).update(is_active=False)
     return HttpResponse('Ok')
