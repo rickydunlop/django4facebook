c60dcb7175f44d6cde2688a79c8711c852e5ec82
Venelin Stoykov <vkstoykov@gmail.com>
2012-10-25 15:21:47 +0200

    Use get_signed_request_data in deauthorize_callback

--- django4facebook/views.py
+++ django4facebook/views.py
@@ -2,8 +2,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
-import facebook
-from .conf import settings
+from .utils import get_signed_request_data
 
 
 @csrf_exempt
@@ -12,12 +11,9 @@ def deauthorize_callback(request):
     When user deauthorize this application from facebook then
     we deactivate the user from our system
     """
-    signed_request = request.REQUEST.get('signed_request')
-    if not signed_request:
+    data = get_signed_request_data(request)
+    if not (data and data.get('user_id')):
         return HttpResponseBadRequest()
-    data = facebook.parse_signed_request(signed_request,
-                                             settings.SECRET_KEY)
-    if not data.get('user_id'):
-        return HttpResponseBadRequest()
+
     User.objects.filter(pk=data['user_id']).update(is_active=False)
     return HttpResponse('Ok')
