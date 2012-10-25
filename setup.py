1c67a3ce1d2695dbac79b57ce41ce53bb25a31b4
Venelin Stoykov <vkstoykov@gmail.com>
2012-10-25 14:11:48 +0200

    Increase version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django4facebook',
-    version='0.1.0',
+    version='0.1.1',
     description='Facebook integration for your Django website.',
     long_description=open('README.md').read(),
     author='Aidan Lister',
