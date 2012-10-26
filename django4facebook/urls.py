116b3da7af0f0ea6c0e26282d21c3602442ebc27
Venelin Stoykov <vkstoykov@gmail.com>
2012-10-26 12:01:22 +0200

    Bug fix

--- django4facebook/urls.py
+++ django4facebook/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('django4facebook',
+urlpatterns = patterns('django4facebook.views',
     url(r'deauthorize/$', 'deauthorize_callback', name='fb_deauthorize_callback'),
 )
